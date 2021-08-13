2aea2773a8548bf01de2a7ceb0fc371d82d4b380
Shivaji Byrapaneni <shivaji.byrapaneni@gmail.com>
2021-08-13 23:14:27 +1000

    Ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:real_app/widgets/chart.dart';
@@ -91,16 +92,31 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     final isLandscape = mediaQuery.orientation == Orientation.landscape;
-    var appBarComponent = AppBar(
-      title: const Text("Finance Tracker"),
-      actions: [
-        IconButton(
-            onPressed: () {
-              _startAddNewTransaction(context);
-            },
-            icon: Icon(Icons.add))
-      ],
-    );
+    PreferredSizeWidget appBarComponent = Platform.isIOS
+        ? CupertinoNavigationBar(
+            middle: Text("Finance Tracker"),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    _startAddNewTransaction(context);
+                  },
+                  child: Icon(CupertinoIcons.add),
+                )
+              ],
+            ),
+          ) as PreferredSizeWidget
+        : AppBar(
+            title: const Text("Finance Tracker"),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    _startAddNewTransaction(context);
+                  },
+                  icon: Icon(Icons.add))
+            ],
+          );
 
     final txListWidget = Container(
         height: (mediaQuery.size.height -
@@ -109,72 +125,79 @@ class _MyHomePageState extends State<MyHomePage> {
             0.7,
         child: TransactionList(_transactions, _deleteTransaction));
 
-    return Scaffold(
-      appBar: appBarComponent,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Platform.isIOS
-          ? Container()
-          : FloatingActionButton(
-              child: Icon(Icons.add),
-              onPressed: () {
-                _startAddNewTransaction(context);
-              },
-            ),
-      body: SingleChildScrollView(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            if (isLandscape)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Switch Chart"),
-                  Switch.adaptive(
-                    activeColor: Theme.of(context).colorScheme.secondary,
-                    value: _showChart,
-                    onChanged: (value) {
-                      setState(() {
-                        _showChart = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            if (!isLandscape)
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Theme.of(context).primaryColorLight,
-                  child: Container(
-                      height: (mediaQuery.size.height -
-                              appBarComponent.preferredSize.height -
-                              mediaQuery.padding.top) *
-                          0.3,
-                      child: Chart(_recentTransactions)),
-                  elevation: 5,
+    var pageBody = SingleChildScrollView(
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          if (isLandscape)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Switch Chart"),
+                Switch.adaptive(
+                  activeColor: Theme.of(context).colorScheme.secondary,
+                  value: _showChart,
+                  onChanged: (value) {
+                    setState(() {
+                      _showChart = value;
+                    });
+                  },
                 ),
+              ],
+            ),
+          if (!isLandscape)
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Theme.of(context).primaryColorLight,
+                child: Container(
+                    height: (mediaQuery.size.height -
+                            appBarComponent.preferredSize.height -
+                            mediaQuery.padding.top) *
+                        0.3,
+                    child: Chart(_recentTransactions)),
+                elevation: 5,
               ),
-            if (!isLandscape) txListWidget,
-            if (isLandscape)
-              _showChart
-                  ? Container(
-                      width: double.infinity,
-                      child: Card(
-                        color: Theme.of(context).primaryColorLight,
-                        child: Container(
-                            height: (mediaQuery.size.height -
-                                    appBarComponent.preferredSize.height -
-                                    mediaQuery.padding.top) *
-                                0.7,
-                            child: Chart(_recentTransactions)),
-                        elevation: 5,
-                      ),
-                    )
-                  : txListWidget
-          ],
-        ),
+            ),
+          if (!isLandscape) txListWidget,
+          if (isLandscape)
+            _showChart
+                ? Container(
+                    width: double.infinity,
+                    child: Card(
+                      color: Theme.of(context).primaryColorLight,
+                      child: Container(
+                          height: (mediaQuery.size.height -
+                                  appBarComponent.preferredSize.height -
+                                  mediaQuery.padding.top) *
+                              0.7,
+                          child: Chart(_recentTransactions)),
+                      elevation: 5,
+                    ),
+                  )
+                : txListWidget
+        ],
       ),
     );
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            child: pageBody,
+            navigationBar: appBarComponent as ObstructingPreferredSizeWidget,
+          )
+        : Scaffold(
+            appBar: appBarComponent,
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: Platform.isIOS
+                ? Container()
+                : FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: () {
+                      _startAddNewTransaction(context);
+                    },
+                  ),
+            body: pageBody,
+          );
   }
 }
